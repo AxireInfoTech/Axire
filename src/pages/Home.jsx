cb87a609a9c5f8ac43a07edaacc681564494e74a
RutvikJR <rachhadiyarutvik09040@gmail.com>
2022-08-27 11:00:32 +0530

    some layout css chagnes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,17 @@ import "../styles/home.scss";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="content">
-        <div className="flex-center">
-          <div className="heading-content">
-            <div className="heading">
-              <p className="main-head">Namaste from AXIRE</p>
-              <p>The place that provides digital solutions to your requirements.</p>
-            </div>
-            <div className="sub-heading">
-              <p>Want a business with us? Just AXIRE it</p>
-            </div>
+    <div className="content">
+      <div className="flex-center">
+        <div className="heading-content">
+          <div className="heading">
+            <p className="main-head">Namaste from AXIRE</p>
+            <p>
+              The place that provides digital solutions to your requirements.
+            </p>
+          </div>
+          <div className="sub-heading">
+            <p>Want a business with us? Just AXIRE it</p>
           </div>
         </div>
       </div>
