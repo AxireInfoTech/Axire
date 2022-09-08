e6fb2cd09e36a04cd5b711d8954c948090e30576
Arth Panchani <59698234+arth40@users.noreply.github.com>
2022-09-08 18:57:32 +0530

    Client screen design added

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import "../styles/clients.scss";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+  <div className="content">
+    <div className="clients-area">
+      <div className="client-part">
+        <h3 class="clients-title">Clients</h3>
+        <div className="client-box"></div>
+        <div className="client-box"></div>
+      </div>
+      <div className="client-part">
+        <p className="review">sakhbdn c sajkfnsaf asfasfas jaskfsa skjfnskjf jsfbfkjsa safjansfkas</p>
+        <p className="reviewer">Man Person-1 : <span>Company-1</span></p>
+      </div>
+      <div className="client-line">
+        <div className="client-box box-hidden"></div>
+        <div className="client-box"></div>
+        <div className="client-box"></div>
+        <div className="client-box"></div>
+        <div className="client-box"></div>
+      </div>
+      <div className="client-line">
+        <div className="client-box box-hidden"></div>
+        <div className="client-box box-hidden"></div>
+        <div className="client-box box-hidden"></div>
+        <div className="client-box"></div>
+        <div className="client-box"></div>
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Clients;
