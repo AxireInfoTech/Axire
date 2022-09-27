7fa95f917ac3cd6cea96d7c8e9b6ed22f8d5eb01
RutvikJR <rachhadiyarutvik09040@gmail.com>
2022-09-27 12:44:38 +0530

    preloading animation gifs added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 import Layout from "./layout/Layout";
 import Routes from "./Routes";
 
 function App() {
+  const [preLoading, setPreLoading] = useState(true);
+  useEffect(() => {
+    const preloadingTimeout = setTimeout(() => {
+      setPreLoading(false);
+    }, 2320);
+  }, []);
+
   return (
-    <Layout>
-      <Routes />
-    </Layout>
+    <>
+      {preLoading && (
+        <div className="preloading-logo">
+          <img style={{ width: "200px" }} src="loading/AXIRE_2.gif" alt="" />
+        </div>
+      )}
+      <Layout>
+        <Routes />
+      </Layout>
+    </>
   );
 }
 
